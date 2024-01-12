fa117eeb4780f9c253c1fff93710a45be56a80f4
tikanoja <tuukka.tikanoja@gmail.com>
2024-01-12 15:17:14 +0200

    add backend fetch test button

--- services/frontend/srcs/js/router.js
+++ services/frontend/srcs/js/router.js
@@ -76,6 +76,10 @@ const locationHandler = async () => {
 	document.getElementById("content").innerHTML = html;
 	document.title = route.title;
 	document.querySelector('meta[name="description"]').setAttribute("content", route.description);
+
+	/*
+		Update event listeners here!
+	*/
 }
 
 // Add an event listener to the window that looks for URL changes
