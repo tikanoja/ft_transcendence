82def2c97ba2bfb496e58c146c261238906a8038
tikanoja <tuukka.tikanoja@gmail.com>
2024-02-21 12:28:45 +0200

    init develop branch and add microservice structure

--- services/backend/pong_service/pong_service/asgi.py
+++ services/backend/pong_service/pong_service/asgi.py
@@ -13,10 +13,10 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from backend.routing import websocket_urlpatterns
+from pong_service.routing import websocket_urlpatterns
 from pong.consumers import PongConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pong_service.settings')
 
 application = ProtocolTypeRouter({
 	'http': get_asgi_application(),
---
+++ services/backend/user_service/pong/consumers.py
@@ -0,0 +1,24 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import asyncio
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+class PongConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
+		logger.info('New connect received')
+		await self.accept()
+		logger.info('New connection established')
+
+	async def disconnect(self, close_code):
+		logger.info('Connection dropped')
+		pass
+	
+	async def receive(self, text_data):
+		logger.info(f'Received text_data: {text_data}')
+		text_data_json = json.loads(text_data)
+		await self.send(text_data=json.dumps({
+			'type': 'connection established',
+			'message': 'you are connected! :)'
+		}))
