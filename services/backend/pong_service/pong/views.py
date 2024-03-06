8bfd43560bfb1e790c133201efbd315a8377be49
sheree.morphett <sheree.morphett@gmail.com>
2024-03-06 14:17:29 +0200

    update

--- services/backend/pong_service/pong/views.py
+++ services/backend/pong_service/pong/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import logging
 
 logger = logging.getLogger(__name__)
@@ -43,12 +45,21 @@ def decrease_number(request):
 		add_cors_headers(response)
 		return response
 
-# this is the placeholder now for scores
-@csrf_exempt 
+
+
+@api_view(['GET'])  # Decorator to specify HTTP methods allowed
 def get_number(request):
 	logger.debug('In get num')
-	global p1Score
-	global p2Score
-	response = JsonResponse({'result': 'success', 'p1Score': p1Score, 'p2Score': p2Score})
-	add_cors_headers(response)
-	return response
+	score = {'p1Score': p1Score, 'p2Score': p2Score}
+	add_cors_headers(score)
+	return Response(score)
+
+
+# # this is the placeholder now for scores
+# @csrf_exempt 
+# def get_number(request):
+# 	logger.debug('In get num')
+# 	global p1Score
+# 	global p2Score
+# 	response = JsonResponse({'result': 'success', 'p1Score': p1Score, 'p2Score': p2Score})
+# 	return response
--- services/backend/pong_service/pong_service/settings.py
+++ services/backend/pong_service/pong_service/settings.py
@@ -28,6 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "https://localhost:8000",
+]
+
 # Application definition
 # Add psycopg2 once implemented PostgreSQL
 # psycopg2-binary==2.9.9 add this to requirements.txt
@@ -35,7 +42,7 @@ INSTALLED_APPS = [
     'channels',
     'pong',
     'daphne',
-
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -52,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'pong_service.urls'
