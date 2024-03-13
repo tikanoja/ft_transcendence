3a8c69b3bb4bc59e062b32047906c1f51bc62474
heikkiseppanen <6179953+heikkiseppanen@users.noreply.github.com>
2024-03-13 16:42:31 +0200

    Made it so chat_service files are mounted as volume for development. Implemented basic chat message relaying

--- services/backend/chat_service/src/chat/consumers.py
+++ services/backend/chat_service/src/chat/consumers.py
@@ -3,33 +3,31 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 tmp_user = ["MrSticks", "User"]
-i = 0;
+i = 0
 
 class ClientConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.groups = [tmp_user[i], "Global"]
-        ++i
-        print(self.groups[0], " connected :O")
         for group in self.groups:
             await self.channel_layer.group_add(group, self.channel_name)
         await self.accept()
+        print(self.groups[0], " connected :O")
 
     async def disconnect(self, close_code):
         for group in self.groups:
             await self.channel_layer.group_discard(group, self.channel_name)
 
     async def receive(self, text_data : str):
+        print(text_data)
         packet = json.loads(text_data)
         await self.channel_layer.group_send(
             "Global",
-            json.dumps({
+            {
                 "type": "chat.message",
                 "message": "[" + self.groups[0] + "] " + packet["message"]
-            })
+            }
         )
 
     async def chat_message(self, event):
-        message  = event["message"]
-        username = event["username"]
-        await self.send(text_data = json.dumps({"sender": username, "message":message}))
+        await self.send(text_data = json.dumps(event))
