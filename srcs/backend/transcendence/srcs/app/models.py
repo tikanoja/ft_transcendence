0cee4315cbd21859a2722b3e699194e612fffec9
Sheree <92857375+ShereeMorphett@users.noreply.github.com>
2024-04-22 12:35:28 +0300

    Update models.py
    deleted time, now import

--- srcs/backend/transcendence/srcs/app/models.py
+++ srcs/backend/transcendence/srcs/app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.db.models.functions import Now
 from django.core.exceptions import ValidationError
 import logging
 from django.db import transaction
