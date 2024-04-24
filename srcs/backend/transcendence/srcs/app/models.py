c9756ebdb8e0359ebe5be57721a27993b99da910
Jenny Boucher <22949834+jboucher154@users.noreply.github.com>
2024-04-24 15:26:34 +0300

    Update models.py
    remove pass for defined functions

--- srcs/backend/transcendence/srcs/app/models.py
+++ srcs/backend/transcendence/srcs/app/models.py
@@ -107,15 +107,13 @@ class PongGameInstance(GameInstance):
     p2_misses = models.IntegerField(default=0)
     p1_score = models.IntegerField(default=0)
     p2_score = models.IntegerField(default=0)
-    pass
-
+    
 
 class ColorGameInstance(GameInstance):
     turns = models.IntegerField(default=0)
     p1_biggest_takeover = models.IntegerField(default=0)
     p2_biggest_takeover = models.IntegerField(default=0)
-    pass
-
+    
 
 # user profile model
 #  language -> maybe add to CustomUser model?
