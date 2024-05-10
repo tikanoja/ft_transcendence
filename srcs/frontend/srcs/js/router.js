33acf82e6ca8e3ece00e1ae559889114a3e15f2f
Jenny Boucher <jboucher154@gmail.com>
2024-05-10 16:11:57 +0300

    fix filepath for chat script, add regex match for profile pages to show not found for extra url infor and match for any refreshed page

--- srcs/frontend/srcs/js/router.js
+++ srcs/frontend/srcs/js/router.js
@@ -13,7 +13,7 @@ document.addEventListener("click", (e) => {
 	// Save the clicked element as target
 	const target = e.target;
 	// Check if the clicked element is a part of the nav anchors
-	if (!target.matches("nav a") || target.id == "profileLinkTag") {
+	if (!target.matches("nav a")) {
 		return ;
 	}
 	// Prevent the navigation to a new page
@@ -117,19 +117,13 @@ const locationHandler = async () => {
 
 	console.log('locationHandler(): matching this to routes: ' + location)
 	// Check the routes (the views above) for a match, if no match: 404
-	const route = routes[location] || routes[404];
-	if (location === '/profile') {
-		let userQuery = await fetch('app/get_current_username/');
-		let username = await userQuery.json()
-		console.log('curent username returned as' + username['message'])
-		if (username['message'] === 'unknown user') {
-			// TODO: take this out and make sure that the routing for the profile requires login. this results in wrong url shown
-			route.view = "app/login/"
-		}
-		else {
-			route.view = "app/profile/" + username['message']
-		}
-
+	let route = routes[location] || routes[404];
+	const profileRe = new RegExp("^/profile/[a-zA-Z1-9]+(/)?$")
+	if (profileRe.test(location)) {
+		route = {
+			view: "/app" + location,
+			title: "Profile | " + pageTitle,
+			description: "PogChamp Profile"}
 	}
 	const querystring = window.location.search;
 	console.log('locationHandler(): fetching this: ' + route.view + querystring);
