1c9310f70aca7089f27ed79bcb514c6e5e1040f9
Timo Poho <timo.poho@outlook.com>
2024-05-17 21:05:33 +0300

    removal of empty line

--- srcs/frontend/srcs/js/pong.js
+++ srcs/frontend/srcs/js/pong.js
@@ -189,7 +189,6 @@ function createGround() {
     return new THREE.Mesh(groundGeometry, groundMaterial);
 }
 
-
 function setup3DScene(scene) {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 
