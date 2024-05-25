aae6b3d8abe3e3f31959461ef25e9335374dfb06
jen <jboucher154@gmail.com>
2024-05-25 15:04:13 +0300

    cleanup router.js

--- srcs/frontend/srcs/js/router.js
+++ srcs/frontend/srcs/js/router.js
@@ -1,14 +1,13 @@
 
 /*
 *** URL-based router for displaying the main content based on the navbar
- */
+*/
 
 const pageTitle = "Pong";
 
-import { updateEventListeners, updateContent } from './index.js'
+import { handleResponseForContentUpdate, updateContent } from './index.js'
 
-// Listens to clicks on the entire document
-//TODO: maybe change this for only the play? everything else will be gone
+
 document.addEventListener("click", (e) => {
 	// Save the clicked element as target
 	const target = e.target;
@@ -21,8 +20,8 @@ document.addEventListener("click", (e) => {
 	route();
 })
 
-// An array of the possible routes (eg. views, pages... etc)
-// * * * * REMEMBER TRAILING / FOR BACKEND CALLS TO AVOID HOURS OF PAIN!!!!! * * * *
+// An object of the possible routes (eg. views, pages... etc)
+// * * * * REMEMBER TRAILING `/` FOR BACKEND CALLS TO AVOID HOURS OF PAIN!!!!! * * * *
 const routes = {
 	404 : {
 		view: "/app/notfound/",
@@ -70,12 +69,13 @@ const routes = {
 // Checks the URL
 const route = (event) => {
 	console.log('In route()');
-	// The event is either the one passed to it, or grab the window event if not (prev / forward buttons)
+	// The event is either the one passed to it, or use the window event if not (prev / forward buttons)
 	event = event || window.event;
 	event.preventDefault();
 	// Update browser history without triggering page reload
 	console.log("route event target : " + event.target.href)
 	console.log("route window location : " + window.location.href)
+	// don't push to history or navigate if we are already there
 	if (event.target.href == window.location.href)
 		return ;
 	console.log('route(): pushing this to history: ' + event.target.href);
@@ -83,28 +83,29 @@ const route = (event) => {
 	locationHandler();
 }
 
+
+//used when redirect (3XX) codes in responses
 const routeRedirect = (target) => {
 	console.log('In routeRedirect()... pushing to history: ' + target);
 	window.history.pushState("", "", target);
 	locationHandler();
 }
 
-// By using 'async' we can use 'await'. This way we can use asynchronous operations without blocking the execution of other code
-const locationHandler = async () => {
-	console.log('In locationHandler()');
-	// Get the path part of URL (eg. https://example.com/friends/profile returns /friends/profile)
-	let location = window.location.pathname;
-	// Redirect https://example.com to https://example.com/ in order to land on home page
+
+function removeExtraLeadingAndTrailingPartsOfLocation(location) {
+
 	if (location.endsWith('/'))
 		location = location.slice(0, -1);
 	if (location.length == 0)
 		location = "/";
 	if (location.startsWith('/app/'))
 		location = location.substring(4);
+	return location;
+}
 
-	console.log('locationHandler(): matching this to routes: ' + location)
-	// Check the routes (the views above) for a match, if no match: 404
-	let route = routes[location] || routes[404];
+
+function checkForCorrectProfilePathRoute(location, route) {
+
 	const profileRe = new RegExp("^/profile/[a-zA-Z0-9]+(/)?$")
 	if (profileRe.test(location)) {
 		route = {
@@ -112,23 +113,34 @@ const locationHandler = async () => {
 			title: "Profile | " + pageTitle,
 			description: "PogChamp Profile"}
 	}
+	return route
+}
+
+
+// match window location to route, fetch the content
+const locationHandler = async () => {
+	console.log('In locationHandler()');
+	
+	let location = window.location.pathname;
+	location = removeExtraLeadingAndTrailingPartsOfLocation(location);
+
+	console.log('locationHandler(): matching this to routes: ' + location);
+	// Check the routes (the views above) for a match, if no match: 404
+	let route = routes[location] || routes[404];
+	console.log("route before profile correction: " + route.view);
+	if (route == routes[404])
+		route = checkForCorrectProfilePathRoute(location, route);
+
+	//send request with querystring
 	const querystring = window.location.search;
 	console.log('locationHandler(): fetching this: ' + route.view + querystring);
-	const response = await fetch(route.view + querystring)//, {redirect: 'manual'});
-	if (!response.ok) {
-		console.log("locationHandler(): Fetch status != OK");
-	} else if (response.redirected) {
-		let redirect_location = response.url;
-        routeRedirect(redirect_location);
-	} else {
-		console.log('locationHandler(): Fetch OK, no redirs, updating site content');
-		const html = await response.text();
-		updateContent(html, route.title, route.description);
-	}
+	const response = await fetch(route.view + querystring);
+	handleResponseForContentUpdate(response, route.title, route.description)
 }
 
 // Add an event listener to the window that looks for URL changes
 window.onpopstate = locationHandler;
+
 // Modify the URL to match the route
 window.route = route;
 
